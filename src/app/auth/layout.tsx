80ac3f9edcd51843855c73c1c945d31975310747
eedu7 <mueedahmad067@gmail.com>
2025-03-28 22:23:32 +0500

    refactor sign-up page layout and form validation, add username and email existence checks

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="mx-auto mt-16 flex max-w-7xl justify-center p-1 md:mt-36 md:p-0">
-            {children}
+            <section className="flex items-center justify-center">{children}</section>
         </main>
     );
 };
--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,7 +1,243 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import { z } from "zod";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { useForm } from "@tanstack/react-form";
+import Link from "next/link";
+import { Divide, LoaderCircle } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+const checkUsernameExists = async (username: string) => {
+    // TODO: add code from this https://github.com/eedu7/my-blog/blob/main/frontend/src/app/(auth)/sign-up/page.tsx
+    if (["tony", "batman", "clark"].includes(username)) {
+        return { message: "Username already exists" };
+    }
+};
+const checkEmailExists = async (email: string) => {
+    // TODO: add code from this https://github.com/eedu7/my-blog/blob/main/frontend/src/app/(auth)/sign-up/page.tsx
+    if (["tony@example.com", "batman@example.com", "clark@example.com"].includes(email)) {
+        return { message: "Email already exists" };
+    }
+};
+
+const SignUpFormSchema = z.object({
+    username: z
+        .string()
+        .min(3, "Username must be at least 3 characters long.")
+        .max(12, "Username must not exceed 12 characters."),
+    email: z.string().email(),
+    password: z
+        .string()
+        .min(8, "Password must be at least 8 characters long.")
+        .max(18, "Password must be at most 18 characters long."),
+});
 const SignUpPage = () => {
-	return <div>SignUpPage</div>;
+    const SIGN_IN_LINK = {
+        href: "/auth/sign-in",
+    };
+    const TERMS_LINK = {
+        href: "/auth/sign-in",
+    };
+
+    const PRIVACY_LINK = {
+        href: "/auth/sign-in",
+    };
+
+    const form = useForm({
+        defaultValues: {
+            username: "",
+            email: "",
+            password: "",
+        },
+        validators: {
+            onChange: SignUpFormSchema,
+        },
+        onSubmit: ({ value }) => {
+            alert(JSON.stringify(value));
+        },
+    });
+    return (
+        <Card className="md:w-96">
+            <CardHeader>
+                <CardTitle>Create Your Account</CardTitle>
+                <CardDescription>Join us today.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                    }}
+                    className="space-y-4"
+                >
+                    <form.Field
+                        name="username"
+                        validators={{
+                            onChangeAsync: async ({ value }) => {
+                                return await checkUsernameExists(value);
+                            },
+                            onChangeAsyncDebounceMs: 500,
+                            onBlurAsync: async ({ value }) => {
+                                return await checkUsernameExists(value);
+                            },
+                            onBlurAsyncDebounceMs: 500,
+                        }}
+                        // eslint-disable-next-line react/no-children-prop
+                        children={(field) => (
+                            <div className="space-y-2">
+                                <Label>Username</Label>
+                                <div className="relative">
+                                    <Input
+                                        onBlur={field.handleBlur}
+                                        onChange={(e) => field.handleChange(e.target.value)}
+                                        type="text"
+                                        className={cn(
+                                            "focus:outline-none",
+                                            field.state.meta.errors.length > 0 && "border-red-500",
+                                        )}
+                                        placeholder="Username"
+                                    />
+                                    {field.state.meta.isValidating && (
+                                        <div className="absolute top-1/2 right-6 -translate-y-1/2 transform">
+                                            <LoaderCircle className="animate-spin text-gray-500" />
+                                        </div>
+                                    )}
+                                    {field.state.meta.errors.length > 0 && (
+                                        <em className="text-xs text-red-500">
+                                            {/* {field.state.meta.errors
+                                                .map((err) => err?.message)
+                                                .join(", ")} */}
+                                            {field.state.meta.errors[0]?.message}
+                                        </em>
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                    />
+                    <form.Field
+                        name="email"
+                        validators={{
+                            onChangeAsync: async ({ value }) => {
+                                return await checkEmailExists(value);
+                            },
+                            onChangeAsyncDebounceMs: 500,
+                            onBlurAsync: async ({ value }) => {
+                                return await checkEmailExists(value);
+                            },
+                            onBlurAsyncDebounceMs: 500,
+                        }}
+                        // eslint-disable-next-line react/no-children-prop
+                        children={(field) => (
+                            <div className="space-y-2">
+                                <Label>Email</Label>
+                                <div className="relative">
+                                    <Input
+                                        onBlur={field.handleBlur}
+                                        onChange={(e) => field.handleChange(e.target.value)}
+                                        type="text"
+                                        className={cn(
+                                            "focus:outline-none",
+                                            field.state.meta.errors.length > 0 && "border-red-500",
+                                        )}
+                                        placeholder="Email"
+                                    />
+                                    {field.state.meta.isValidating && (
+                                        <div className="absolute top-1/2 right-6 -translate-y-1/2 transform">
+                                            <LoaderCircle className="animate-spin text-gray-500" />
+                                        </div>
+                                    )}
+                                    {field.state.meta.errors.length > 0 && (
+                                        <em className="text-xs text-red-500">
+                                            {/* {field.state.meta.errors
+                                                .map((err) => err?.message)
+                                                .join(", ")} */}
+                                            {field.state.meta.errors[0]?.message}
+                                        </em>
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                    />
+                    <form.Field
+                        name="password"
+                        // eslint-disable-next-line react/no-children-prop
+                        children={(field) => (
+                            <div className="space-y-2">
+                                <Label>Password</Label>
+                                <div>
+                                    <Input
+                                        onBlur={field.handleBlur}
+                                        onChange={(e) => field.handleChange(e.target.value)}
+                                        type="password"
+                                        className={cn(
+                                            "focus:outline-none",
+                                            field.state.meta.errors.length > 0 && "border-red-500",
+                                        )}
+                                        placeholder="Password"
+                                    />
+                                    {field.state.meta.errors.length > 0 && (
+                                        <em className="text-xs text-red-500">
+                                            {field.state.meta.errors
+                                                .map((err) => err?.message)
+                                                .join(", ")}
+                                        </em>
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                    />
+                </form>
+            </CardContent>
+            <CardFooter>
+                <div className="flex w-full flex-col space-y-2">
+                    <div className="w-full">
+                        {/* TODO: Do not use hard coded value, make a varialbe */}
+                        <Link className="float-end text-sm text-blue-800" href={SIGN_IN_LINK.href}>
+                            Already have an account? Sign in
+                        </Link>
+                    </div>
+                    <Button
+                        variant="outline"
+                        className="w-full cursor-pointer"
+                        onClick={() => form.handleSubmit()}
+                    >
+                        Sign Up
+                    </Button>
+                    <div>
+                        <p className="text-[0.8rem] text-gray-600">
+                            Click &quot;Register&quot; to agree to MyBlog&apos;s{" "}
+                            <Link
+                                className="text-blue-800 underline underline-offset-2"
+                                href={TERMS_LINK.href}
+                            >
+                                Terms of Service
+                            </Link>{" "}
+                            and acknowledge that MyBlog&apos;s{" "}
+                            <Link
+                                className="text-blue-800 underline underline-offset-2"
+                                href={PRIVACY_LINK.href}
+                            >
+                                Privacy Policy
+                            </Link>{" "}
+                            applies to you.
+                        </p>
+                    </div>
+                </div>
+            </CardFooter>
+        </Card>
+    );
 };
 
 export default SignUpPage;
