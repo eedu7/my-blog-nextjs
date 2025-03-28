920cdb6bed466c2e7805f71496f27cc63ef33e8a
eedu7 <mueedahmad067@gmail.com>
2025-03-28 21:31:59 +0500

    add Prettier and Tailwind CSS plugin, update auth layout styling, and configure VSCode settings

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-	return <div>{children}</div>;
+    return (
+        <main className="mx-auto mt-16 flex max-w-7xl justify-center p-1 md:mt-36 md:p-0">
+            {children}
+        </main>
+    );
 };
 
 export default AuthLayout;
