14bd9eeb9d9da05f08fdb6a7facde5afba5a1c89
eedu7 <mueedahmad067@gmail.com>
2025-04-01 18:47:44 +0500

    Added a function to save the user data

--- src/modules/authentication/hooks/useAuth.ts
+++ src/modules/authentication/hooks/useAuth.ts
@@ -2,21 +2,31 @@ import { registerUser } from "./../api/auth.api";
 import { useMutation } from "@tanstack/react-query";
 import { loginUser } from "../api/auth.api";
 import { setCookie } from "cookies-next/client";
+import { AuthResponse } from "@/modules/authentication/api/auth.types";
+
+const saveAuthData = (data: AuthResponse) => {
+    setCookie("_JWT_ACCESS_TOKEN", data.token.access_token, {
+        maxAge: data.token.expiry_minutes * 60, // Converting into seconds
+        secure: true,
+        sameSite: "strict",
+    });
+    setCookie("_JWT_REFRESH_TOKEN", data.token.refresh_token, {
+        secure: true,
+        sameSite: "strict",
+    });
+    setCookie("_USER_DATA", JSON.stringify(data.user), {
+        secure: true,
+        sameSite: "strict",
+        maxAge: data.token.expiry_minutes * 60,
+    });
+};
 
 export const useAuth = () => {
     const login = useMutation({
         mutationFn: loginUser,
         mutationKey: ["loginUser"],
         onSuccess: (data) => {
-            setCookie("_JWT_ACCESS_TOKEN", data.token.access_token, {
-                maxAge: data.token.expiry_minutes * 60, // Converting into seconds
-                secure: true,
-                sameSite: "strict",
-            });
-            setCookie("_JWT_REFRESH_TOKEN", data.token.refresh_token, {
-                secure: true,
-                sameSite: "strict",
-            });
+            saveAuthData(data);
         },
         onError: (error) => {
             console.error("Login failed!", error);
@@ -27,15 +37,7 @@ export const useAuth = () => {
         mutationFn: registerUser,
         mutationKey: ["registerUser"],
         onSuccess: (data) => {
-            setCookie("_JWT_ACCESS_TOKEN", data.token.access_token, {
-                maxAge: data.token.expiry_minutes * 60, // Converting into seconds
-                secure: true,
-                sameSite: "strict",
-            });
-            setCookie("_JWT_REFRESH_TOKEN", data.token.refresh_token, {
-                secure: true,
-                sameSite: "strict",
-            });
+            saveAuthData(data);
         },
         onError: (error) => {
             console.error("Registration failed!", error);
